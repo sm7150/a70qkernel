f9d621064014dd8496b3c8fcc10b0b43f4138c8b
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-12-08 22:31:30 +0300

    schedutil: add task_is_booster() protection to schedutil tuneables
    Signed-off-by: Julian Liu <wlootlxt123@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -671,6 +671,9 @@ static ssize_t up_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
+	if (task_is_booster(current))
+		return count;
+
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
 
@@ -690,6 +693,9 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
+
+	if (task_is_booster(current))
+		return count;
 
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
@@ -716,6 +722,9 @@ static ssize_t hispeed_load_store(struct gov_attr_set *attr_set,
 {
 	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
 
+	if (task_is_booster(current))
+		return count;
+
 	if (kstrtouint(buf, 10, &tunables->hispeed_load))
 		return -EINVAL;
 
@@ -739,6 +748,9 @@ static ssize_t hispeed_freq_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned long hs_util;
 	unsigned long flags;
+
+	if (task_is_booster(current))
+		return count;
 
 	if (kstrtouint(buf, 10, &val))
 		return -EINVAL;
@@ -768,6 +780,9 @@ static ssize_t pl_store(struct gov_attr_set *attr_set, const char *buf,
 {
 	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
 
+	if (task_is_booster(current))
+		return count;
+
 	if (kstrtobool(buf, &tunables->pl))
 		return -EINVAL;
 
@@ -787,6 +802,10 @@ static ssize_t iowait_boost_enable_store(struct gov_attr_set *attr_set,
 {
 	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
 	bool enable;
+
+	if (task_is_booster(current))
+		return count;
+
 	if (kstrtobool(buf, &enable))
 		return -EINVAL;
 
