bd8ec4a7807e4c7db3542eb12ba2fcfefb64cfed
Jaegeuk Kim <jaegeuk@kernel.org>
2020-05-08 12:25:45 -0700

    f2fs: remove blk_plugging in block_operations
    blk_plugging doesn't seem to give any benefit.
    
    Change-Id: Ia187efe2617823492dabc4b1f77e0fae43c68fab
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Git-commit: 1f5f11a3c41e2b23288b2769435a00f74e02496b
    Git-repo: https://git.kernel.org/pub/scm/linux/kernel/git/jaegeuk/f2fs.git/
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>
    (cherry picked from commit 7d5279ab6acb1457feac493c407ed01715d04eef)
    (cherry picked from commit aae2d2df70e3e7e3e9fbd202b5e24a0ffa34d788)

--- fs/f2fs/checkpoint.c
+++ fs/f2fs/checkpoint.c
@@ -1117,10 +1117,7 @@ static int block_operations(struct f2fs_sb_info *sbi)
 		.nr_to_write = LONG_MAX,
 		.for_reclaim = 0,
 	};
-	struct blk_plug plug;
 	int err = 0, cnt = 0;
-
-	blk_start_plug(&plug);
 
 retry_flush_quotas:
 	if (__need_flush_quota(sbi)) {
@@ -1153,8 +1150,7 @@ retry_flush_dents:
 		f2fs_unlock_all(sbi);
 		err = f2fs_sync_dirty_inodes(sbi, DIR_INODE);
 		if (err)
-			goto out;
-		blk_flush_plug(current);
+			return err;
 		cond_resched();
 		goto retry_flush_quotas;
 	}
@@ -1176,8 +1172,7 @@ retry_flush_dents:
 		f2fs_unlock_all(sbi);
 		err = f2fs_sync_inode_meta(sbi);
 		if (err)
-			goto out;
-		blk_flush_plug(current);
+			return err;
 		cond_resched();
 		goto retry_flush_quotas;
 	}
@@ -1193,7 +1188,7 @@ retry_flush_nodes:
 		if (err) {
 			up_write(&sbi->node_change);
 			f2fs_unlock_all(sbi);
-			goto out;
+			return err;
 		}
 		blk_flush_plug(current);
 		cond_resched();
@@ -1206,8 +1201,6 @@ retry_flush_nodes:
 	 */
 	__prepare_cp_block(sbi);
 	up_write(&sbi->node_change);
-out:
-	blk_finish_plug(&plug);
 	return err;
 }
 
