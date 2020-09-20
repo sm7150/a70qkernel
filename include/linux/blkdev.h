55a1fdcf03fcd701c4c5889ffece4a2cbc0780b2
HolyAngel <slverwolf@gmail.com>
2020-09-19 22:28:50 -0500

    include: blkdev.h: Double MAX_RQ amount
    This double the nr_requests amount, which is the amount of requests we can write to the disc. Since we have UFS storage, we can handle this.

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -46,11 +46,7 @@ struct blk_queue_stats;
 struct blk_stat_callback;
 
 #define BLKDEV_MIN_RQ	4
-#ifdef CONFIG_LARGE_DIRTY_BUFFER
-#define BLKDEV_MAX_RQ	256
-#else
-#define BLKDEV_MAX_RQ  128     /* Default maximum */
-#endif
+#define BLKDEV_MAX_RQ	256	/* Default maximum */
 
 /* Must be consisitent with blk_mq_poll_stats_bkt() */
 #define BLK_MQ_POLL_STATS_BKTS 16
