fbab24856a0488865a3b04908fdead3cb9b96999
firemax13 <mydadisbrave2@gmail.com>
2020-10-23 04:54:20 +0000

    Hub: ReleaseTool: Rework & Revised
    Signed-off-by: firemax13 <mydadisbrave2@gmail.com>

--- hub_release.sh
+++ hub_release.sh
@@ -9,7 +9,8 @@ EDITION="FrostEdition"
 ED="FE"
 RELEASE_NAME="FireKernel Official Rev $VER $EDITION for Galaxy A70"
 TAG="$VER$ED"
+TAG_COMMIT="039fd38777b77e5dd6bd05d63dd7a37dc2d5f561"
 
 git remote -v
 
-hub release create -a $(pwd)/AnyKernel3/*.zip -m "$RELEASE_NAME" $TAG_COMMIT $TAG
+hub release create -a $(pwd)/AnyKernel3/*.zip -m "$RELEASE_NAME" -t $TAG_COMMIT $TAG
