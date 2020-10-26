8924bbff8aca0990b6b2c3bea1b7b21c55cfa371
firemax13 <mydadisbrave2@gmail.com>
2020-10-26 08:41:17 +0000

    Hub-Release: Script: Improved it & Re-Initialize
    Signed-off-by: firemax13 <mydadisbrave2@gmail.com>

--- hub_release.sh
+++ hub_release.sh
@@ -1,16 +1,23 @@
 #!bin/bash
+# Custom Hub Release Actions for File Releasing
+# Copyright 2021, firemax13@github.com
 
 set -e
 
 # For Hub Release Action & Less efforts
 
+echo " "
+echo "Setting Up Proper Release Name"
 VER="3.3"
 EDITION="FrostExtremeEdition"
 ED="FEE"
 RELEASE_NAME="FireKernel Official Rev $VER $EDITION for Galaxy A70"
 TAG="$VER$ED"
 TAG_COMMIT="$1"
+echo " "
 
-git remote -v
+git status
+echo " "
 
+echo "Releasing..."
 hub release create -a $(pwd)/AnyKernel3/*.zip -m "$RELEASE_NAME" -t $TAG_COMMIT $TAG
