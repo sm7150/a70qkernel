704089b804108e355a7f9ec6ce1a732fedefe049
firemax13 <mydadisbrave2@gmail.com>
2020-11-04 01:41:40 +0000

    Update: Fire Kernel Revision 3.7 Beta FrostExtremeEdition
    Signed-off-by: firemax13 <mydadisbrave2@gmail.com>

--- hub_release.sh
+++ hub_release.sh
@@ -8,7 +8,7 @@ set -e
 
 echo " "
 echo "Setting Up Proper Release Name"
-VER="3.5"
+VER="3.7"
 EDITION="FrostExtremeEdition"
 ED="FEE"
 RELEASE_NAME="FireKernel Official Rev $VER $EDITION for Galaxy A70"
@@ -19,12 +19,12 @@ echo " "
 git status
 echo " "
 
-echo "Release without commit hashes"
 if [ "$1" == "no-commit" ]; then
+	echo "Release without commit hashes"
         hub release create -a $(pwd)/AnyKernel3/*.zip -m "$RELEASE_NAME" $TAG
 fi
 
-echo "Releasing with commit hashes"
 if [ "$1" == "$TAG_COMMIT" ]; then
+	echo "Releasing with commit hashes"
 	hub release create -a $(pwd)/AnyKernel3/*.zip -m "$RELEASE_NAME" -t $TAG_COMMIT $TAG
 fi
