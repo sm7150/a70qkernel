b4b9134936009a0e01fc0dc8e4fb38108a12741f
firemax13 <mydadisbrave2@gmail.com>
2020-11-05 06:14:29 +0000

    Update: Fire Kernel Revision 3.9 FrostExtremeEdition
    -
    - Built With Latest Google GCC Toolchain With 11.0_r11 Rev
    - Built With Performance Google Clang Optimizations
    - GCC & Clang Forced To Compile Performance Parameters
    
    -
    - Linux Kernel Version: 4.14.183 Upstreamed CAF
    - Fire Kernel Linux Edition: xZarch Custom Edition
    - Latest LA.UM.9.1.r1-07300-SMxxx0.0 CAF TAG Merged
    -
    
    -
    - Fire Kernel Build Status: Official & Active
    - Fire Kernel Edition: FrostExtremeEdition
    -
    
    - EAS & HMP Improved & Optimized
    - Stable EAS & HMP Certified & Clarified
    - DEVFreq Boost Improved & Enabled
    - CPUInput Boost Optimized & Enabled
    - Midly Improved CPU WakeUp & Sleep
    - Thread Marked As Performance Critical
    - I/O Scheduler CFQ Optimized & Improved
    - I/O Scheduler Lag Deleted & Resolved
    - Disabled Some EAS Idling Prefer for Optimizations
    - Added CRC32 Speedy Module & Enabled it
    - Increased Some Allocations for Speedy Boot
    - Increased Some Parameters for Optimizations
    - Zygot Kick Boost Added for Performance Tasking
    - Added DDR Boost For Frame Lag-Free
    - Disable & Decreased Lots of Useless Debuggings
    - Speedy Cache Allocations
    - Fixed Some Leaking for Any Modules & Parameters
    - DEBUG_FS Disabled for Better Performance
    - Printk Decrased for Less CPU Hugging
    - Resolved A Lots Of Wrong Calculations
    - Wireless Debugging Decreased
    - Now Support All Kernel Manager Apps
    - Support Kernel Manager Flashing
    - Now Support Latest Magisk Any Types
    - Kernel Audit Furthered Removed
    - TCP Westwood Default for Better Performance
    - Midly Improved Battery Handlings
    -
    - Merged Samsung BTH4 Kernel Patches
    
    Signed-off-by: priyesh2 <priyesh241997@gmail.com>

--- hub_release.sh
+++ hub_release.sh
@@ -8,23 +8,32 @@ set -e
 
 echo " "
 echo "Setting Up Proper Release Name"
-VER="3.7"
+VER="3.9"
 EDITION="FrostExtremeEdition"
 ED="FEE"
 RELEASE_NAME="FireKernel Official Rev $VER $EDITION for Galaxy A70"
 TAG="$VER$ED"
-TAG_COMMIT="$1"
+TAG_COMMIT="$2"
 echo " "
+
+if [ "$1" == "-recursive" ]; then
+	echo "Recursive Mode Activated"
+	echo " "
+fi
+
+if [ "$1" == "$2" ]; then
+	^C
+fi
 
 git status
 echo " "
 
-if [ "$1" == "no-commit" ]; then
+if [ "$3" == "-no-commit" ]; then
 	echo "Release without commit hashes"
         hub release create -a $(pwd)/AnyKernel3/*.zip -m "$RELEASE_NAME" $TAG
 fi
 
-if [ "$1" == "$TAG_COMMIT" ]; then
+if [ "$2" == "$TAG_COMMIT" ]; then
 	echo "Releasing with commit hashes"
 	hub release create -a $(pwd)/AnyKernel3/*.zip -m "$RELEASE_NAME" -t $TAG_COMMIT $TAG
 fi
