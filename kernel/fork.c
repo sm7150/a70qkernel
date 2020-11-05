d20cfeddfdc82607c000469d9c863aa2e2d9eedf
firemax13 <mydadisbrave2@gmail.com>
2020-11-05 04:21:22 +0000

    kernel/fork.c: Removed Non-Included Includes
    Signed-off-by: firemax13 <mydadisbrave2@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -91,8 +91,6 @@
 #include <linux/livepatch.h>
 #include <linux/thread_info.h>
 #include <linux/cpufreq_times.h>
-#include <linux/scs.h>
-#include <linux/simple_lmk.h>
 #include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
 
