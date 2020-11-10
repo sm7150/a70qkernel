e8f00781b398e2c66fd7e81f0ae5b0ff09fa6e3b
Sultan Alsawaf <sultan@kerneltoast.com>
2020-11-10 02:34:10 +0000

    [BACKPORT] kernel: Improve performance critical IRQ framework
    Instead of letting performance critical IRQs just pin themselves to the
    first CPU in cpu_perf_mask, implement a simple balancer to distribute
    the IRQs across all performance CPUs.
    
    In addition, use __GFP_NOFAIL since the required memory allocation is
    small and this is an important feature. Clean up superfluous *irqsave
    locking in contexts where IRQs are already disabled as well.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: firemax13 <mydadisbrave2@gmail.com>

--- kernel/irq/manage.c
+++ kernel/irq/manage.c
@@ -1170,6 +1170,7 @@ static void affine_one_perf_irq(struct irq_desc *desc)
 	int cpu;
 
 	/* Balance the performance-critical IRQs across all perf CPUs */
+	get_online_cpus();
 	while (1) {
 		cpu = cpumask_next_and(perf_cpu_index, cpu_perf_mask,
 				       cpu_online_mask);
@@ -1178,7 +1179,7 @@ static void affine_one_perf_irq(struct irq_desc *desc)
 		perf_cpu_index = -1;
 	}
 	irq_set_affinity_locked(&desc->irq_data, cpumask_of(cpu), true);
-
+	put_online_cpus();
 	perf_cpu_index = cpu;
 }
 
@@ -1523,6 +1524,9 @@ __setup_irq(unsigned int irq, struct irq_desc *desc, struct irqaction *new)
 		/* Set default affinity mask once everything is setup */
 		if (new->flags & IRQF_PERF_CRITICAL)
 			setup_perf_irq_locked(desc);
+			raw_spin_lock(&perf_irqs_lock);
+			affine_one_perf_irq(desc);
+			raw_spin_unlock(&perf_irqs_lock);
 
 	} else if (new->flags & IRQF_TRIGGER_MASK) {
 		unsigned int nmsk = new->flags & IRQF_TRIGGER_MASK;
